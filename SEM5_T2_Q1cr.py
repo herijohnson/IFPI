edd9ff4c5df4c667eb131efa8b3db1a119c9d0b2
Johnson <68112110+herijohnson@users.noreply.github.com>
2025-09-27 22:09:02 +0000

    comentários

--- SEM5_T2_Q1cr.py
+++ SEM5_T2_Q1cr.py
@@ -1,5 +1,11 @@
-letra = input()
+def vogal(letra):
+    # Verifica se é uma vogal maíuscula ou mínuscula
+    print(letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u' or letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U')
 
-letra = (a,e,i,o,u)
 
-print(letra)
+def main():
+# Entrada de dado e verifica o primeiro caractere
+    letra = input("Digite uma letra: ")[0]
+    vogal(letra)
+if __name__ == "__main__":
+   main()
